9438073169b0f7e8ff28136063bf2c46e0cb6ab5
Kristin Jeanna <67802120+kristinjeanna@users.noreply.github.com>
2020-07-03 14:04:38 -0500

    Initial import.

---
+++ README.md
@@ -0,0 +1,21 @@
+# dir-to-ro-dmg
+
+A bash script to create a read-only DMG containing the contents of a directory. For Mac only.
+
+## Usage
+
+The script requires three arguments:
+
+ - The path to the directory to be processed
+ - The volume name (i.e., the name of the volume shown in Finder when mounted)
+ - The filename to be given to the created DMG file
+
+## Example of Usage
+
+### Creating the volume
+
+![screenshot](images/run-example.png)
+
+### The volume, when mounted
+
+![screenshot](images/mounted-volume.png)
---
+++ dir-to-ro-dmg.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+EXPECTED_ARGS=3
+
+if [ $# -ne $EXPECTED_ARGS ]; then
+  echo "Incorrect args: $#"
+  echo "Usage: $0 {dir to archive} {volume name} {volume filename}"
+  for var in "$@"; do
+    echo $var
+  done
+  exit 1
+fi
+
+# size of directory in MB
+SIZE=`du -BM -s $1 | awk '{print $1}' | sed -e 's/M//'`
+SIZE=$((SIZE*2))
+
+# create the read-write filesystem
+hdiutil create -megabytes ${SIZE} -type SPARSE -fs HFS+J -volname $2 $3
+
+# attach it
+hdiutil attach $3.sparseimage
+
+# copy content to it
+cp -R $1 /Volumes/$2/
+
+# detach it
+DEVICE=`mount | grep $2 | awk '{print $1}'`
+hdiutil detach ${DEVICE}
+
+# compact it (which may or may not have much effect)
+hdiutil compact $3.sparseimage -batteryallowed
+
+# convert RW sparseimage to RO image
+hdiutil convert $3.sparseimage -format UDRO -o $3
+rm $3.sparseimage
